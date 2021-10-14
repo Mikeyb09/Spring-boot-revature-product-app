af433b22f53e4f52f1a205219ec1d36808c909fd
James Barlowe <m_barlowe@hotmail.com>
2021-10-14 10:58:49 -0400

    third commit

--- src/main/java/com/example/springbootrevatureproductapp/controller/EmployeeController.java
+++ src/main/java/com/example/springbootrevatureproductapp/controller/EmployeeController.java
@@ -1,5 +1,12 @@
 package com.example.springbootrevatureproductapp.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+	
+	public void display() {
+		
+	}	
 
 }
