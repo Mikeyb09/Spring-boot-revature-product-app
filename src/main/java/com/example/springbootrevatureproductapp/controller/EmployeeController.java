e2b393a63a67957cb22511339ec9e1fc48050b4f
James Barlowe <m_barlowe@hotmail.com>
2021-10-14 11:07:12 -0400

    made additions to EmployeeController

--- src/main/java/com/example/springbootrevatureproductapp/controller/EmployeeController.java
+++ src/main/java/com/example/springbootrevatureproductapp/controller/EmployeeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 	
 	public void display() {
-		
+		System.out.println("Hello i am James");
 	}	
 
 }
